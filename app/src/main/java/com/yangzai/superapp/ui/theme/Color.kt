4f7d9e806f961484792b69855de5b3667cbea0be
xuyang <307315148@qq.com>
2024-03-06 11:44:58 +0800

    1

--- app/src/main/java/com/yangzai/superapp/ui/theme/Color.kt
+++ app/src/main/java/com/yangzai/superapp/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.theme
+package com.yangzai.superapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/test/java/com/yangzai/superapp/ExampleUnitTest.kt
+++ app/src/test/java/com/yangzai/superapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.yangzai.superapp
 
 import org.junit.Test
 
